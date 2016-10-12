4c55fa74255e3f3bd34312e523ba012b02de22be
shenlvmeng <xin_yuanming@163.com>
2016-10-12 15:36:46 +0800

    add designed scrollbar to three areas

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,14 @@
 $().ready(function(){
-	//redesign scrollbar
-	$('html').perfectScrollbar();
-	$("#user_list").perfectScrollbar();
-	$("#chat_content").perfectScrollbar();
-	$("#say_something textarea").perfectScrollbar();
 	var socket = io();
 	var name = Cookies.get('user');
 	var to = "all";
 	var timenow = 0;
 	var timemark = null;
 	var isCtrl = false;
+	//redesign scrollbar
+	var sc = appendScrollbar($("#chat_content"), "c");
+	var su = appendScrollbar($("#user_list"), "u");
+	var ss = appendScrollbar($("#say_something"), "s");
 
 	socket.emit('online', {name: name});
 	$("#user_id").html(name);
@@ -41,9 +40,11 @@ $().ready(function(){
 		if(data.to == "all" && to == "all"){
 			p_q.find("div.others").before("<div class='avatar-name'>"+data.from+"</div>");
 			$("#chat_content").append(p_q);
+			updateScrollbar("c");
 		} else if(data.to == name) {
 			//TODO:some notification
 			$("#chat_content").append(p_q);
+			updateScrollbar("c");
 		}
 		//extra height: name--10px(content) + 5px(margin), div.others--{content height} + 2px(border) + 10px(padding)
 		p_q.height(p_q.find("div.others").height() + 27 || 57);
@@ -76,7 +77,7 @@ $().ready(function(){
 		$('#user_list ul').empty();
 		var len = 0;
 		$("#user_info img").attr("src", "pic/pic"+(parseInt(users[name], 16)+1)+".jpg");
-		$('#user_list ul').append('<li title="all" class="active"><img src="pic/room.jpg" /><div class="user_name">所有人</div></li>')
+		$('#user_list ul').append('<li title="all" class="active"><img src="pic/room.jpg" /><div class="user_name">所有人</div></li>');
 		for(var i in users){
 			$('#user_list ul').append("<li title='"+i+"'><img src='pic/pic"+(parseInt(users[i], 16)+1)+".jpg' /><div class='user_name'>"+i+"</div></li>");
 			len++;
@@ -92,6 +93,7 @@ $().ready(function(){
 				retarget(len);
 			}
 		});
+		updateScrollbar("u");
 	}
 
 	//refresh chat target
@@ -118,6 +120,36 @@ $().ready(function(){
 		return time;
 	}
 
+	//simulate scroll bar
+	sc.bind('wheel', function(e){
+		if (e.originalEvent.deltaY < 0) {
+        	sc.scrollTop(Math.max(sc.scrollTop() + e.originalEvent.deltaY), 0);
+	    }
+	    else {
+	        sc.scrollTop(Math.min(sc.scrollTop() + e.originalEvent.deltaY), sc.prop("scrollHeight") - sc.prop("clientHeight"));
+	    }
+	    sc.find(".scrollbar").css("margin-top", sc.scrollTop() * 460 / sc.prop("scrollHeight"));
+	});
+	su.bind('wheel', function(e){
+		if (e.originalEvent.deltaY < 0) {
+        	su.scrollTop(Math.max(su.scrollTop() + e.originalEvent.deltaY), 0);
+	    }
+	    else {
+	        su.scrollTop(Math.min(su.scrollTop() + e.originalEvent.deltaY), su.prop("scrollHeight") - su.prop("clientHeight"));
+	    }
+	    su.find(".scrollbar").css("margin-top", su.scrollTop() * 665 / su.prop("scrollHeight"));
+	});
+	var sst = ss.find("textarea");
+	sst.bind('wheel', function(e){
+		if (e.originalEvent.deltaY < 0) {
+        	sst.scrollTop(Math.max(sst.scrollTop() + e.originalEvent.deltaY / 2), 0);
+	    }
+	    else {
+	        sst.scrollTop(Math.min(sst.scrollTop() + e.originalEvent.deltaY / 2), sst.prop("scrollHeight") - sst.prop("clientHeight"));
+	    }
+	    ss.find(".scrollbar").css("margin-top", sst.scrollTop() * 132 / sst.prop("scrollHeight"));
+	});
+
 	$("#action").click(function(){
 		var msg = $("#say_something textarea").val();
 		if(msg.trim() == "") return false;
@@ -132,16 +164,20 @@ $().ready(function(){
 		p.find("img").attr("src", $("#user_info img").attr("src"));
 		//extra height: div.self--{content height} + 10px(padding)
 		p.height(p.find("div.self").height() + 10 || 40);
+		updateScrollbar("c");
 
 		socket.emit('chat', {from: name, to: to, msg: msg});
 		//clear input field
 		$("#say_something textarea").val("").focus();
+		updateScrollbar("s");
 	});
 
 	$("#say_something textarea").keydown(function(e){
 		if(e.keyCode == 17){
 			isCtrl = true;
 			e.preventDefault();
+		} else if(e.keyCode > 36 && e.keyCode < 41){
+			ss.find(".scrollbar").css("margin-top", ss.find("textarea").scrollTop() * 132 / ss.find("textarea").prop('scrollHeight'));
 		}
 	});
 
@@ -151,6 +187,7 @@ $().ready(function(){
 				$("#say_something textarea").val(function(i, text) {
     				return text + "\r\n";
 				});
+				updateScrollbar("s");
 			} else {
 				$("#action").trigger("click");
 				e.preventDefault();
@@ -159,6 +196,9 @@ $().ready(function(){
 			isCtrl = false;
 		}
 	});
+	$("#say_something textarea").on('input selectionchange propertychange', function(){
+		updateScrollbar("s");
+	})
 
 	$(window).keydown(function (e) {
 		if (e.keyCode == 116) {
@@ -169,4 +209,46 @@ $().ready(function(){
 			}
 		}
 	});
+
+	//update scroll height
+	function updateScrollbar(id){
+		var sh = 0;
+		var ch = 0;
+		if(id == "c"){
+			sh = sc.prop('scrollHeight');
+			ch = sc.prop('clientHeight');
+			sc.find(".scrollbar").height(460 * ch / sh);
+		} else if(id == "u"){
+			sh = su.prop('scrollHeight');
+			ch = su.prop('clientHeight');
+			su.find(".scrollbar").height(665 * ch / sh);
+		} else if(id == "s"){
+			sh = ss.find("textarea").prop('scrollHeight');
+			ch = ss.find("textarea").prop('clientHeight');
+			ss.find(".scrollbar").height(132 * ch / sh);
+			if(sh > ch){
+				ss.find(".scrollbar").css("margin-top", (sh - ch) * 132 / sh);
+			} else {
+				ss.find(".scrollbar").css("margin-top", ss.find("textarea").scrollTop() * 132 / sh);
+			}
+		}
+	}
+
+	function appendScrollbar(container, id){
+		//container is a jQuery object
+		container.addClass("scrollContainer");
+		//height of the field
+		var c = container.prop('clientHeight');
+		if(id == "c"){
+			var cc = c - 20;
+		} else if(id == "u") {
+			var cc = c - 10;
+		} else if(id == "s") {
+			var cc = c - 5;
+		}
+		var barHeight = cc * c / container.prop('scrollHeight');
+		
+		container.prepend("<div class='scrollfield' style='height: "+cc+"px' data-sid='"+id+"'><div class='scrollbar' style='margin-top: 0px; height: "+Math.min(barHeight, cc)+"px'></div></div>");
+		return container;
+	}
 });
